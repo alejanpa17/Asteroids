09ec40e03e8e0b0cb0ce1c4726bec0a0b6faa0d9
Aynil98 <aliciaromeragomez@gmail.com>
2019-11-09 18:01:58 +0100

    he hecho las dos funciones aunque no estoy segura de si son así, sobre todo la de la velocidad

--- functions.cpp
+++ functions.cpp
@@ -1,8 +1,11 @@
 
 
 /*Librerias*/
+#include <iostream>
 #include <math.h> /*Libreria para operaciones matematicas*/
-
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
 
 /*Clase de los cuerpos celestiales*/
 #include "object.cpp"
@@ -16,6 +19,7 @@
 #define HEIGHT 200;
 #define M 1000;
 #define SDM 50;
+const double PI = 3.14159265;
 
 
 /*COMENTARIOS: si vais a cambiar los atributos que recibe la funcion decidmelo porque
@@ -46,7 +50,25 @@ void forces(double distance, object asteroid, object element, double angle, doub
 
 /*Calcula la aceleracion de un asteroide para un eje,
 debe de valer para cualquier eje y sumar la force que ya tiene el asteroide mas la que calculemos*/ /*ALICIA*/
-double acceleration(double weight, double force){
+// double acceleration(double weight, double force){
+//
+//         return 0;
+//
+// }
+
+double acceleration(object asteroid, double force, double angle){
+        //la fuerza cuando se aplica con un angulo tiene componente de coseno y seno
+        double sin_angle = sin(angle*PI/180);
+        double cos_angle = cos(angle*PI/180);
+
+        double sum_forces_x = asteroid.force_x + (force*cos_angle);
+        double sum_forces_y = asteroid.force_y + (force*sin_angle);
+
+        double acceleration_x = (1/asteroid.weight)*sum_forces_x;
+        double acceleration_y = (1/asteroid.weight)*sum_forces_y;
+
+        //imprimir para comprobar los valores
+        cout<<"aceleration x: "<<acceleration_x<< "\n" <<"aceleration y: "<<acceleration_y<< endl;
 
         return 0;
 
@@ -54,6 +76,11 @@ double acceleration(double weight, double force){
 
 /*Calcula la velocidad de un asteroide para un eje, debe de valer para ambos ejes a la vez*/ /*ALICIA*/
 void speed(object asteroid, double acceleration_x, double acceleration_y ){
+  //no se si está bien hecho, no se a que se refiere con "debe de valer para ambos ejes a la vez"
+        double n_speed_x = asteroid.speed_x + acceleration_x*T;
+        double n_speed_y = asteroid.speed_y + acceleration_y*T;
+        asteroid.speed_x = n_speed_x;
+        asteroid.speed_y = n_speed_y;
 
         return;
 
