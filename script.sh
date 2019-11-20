9f778bd7005cc42776e4c6d6163e1e0e1e5a0641
alejanpa17 <alejandro170999@gmail.com>
2019-11-20 10:51:48 +0100

    Pruebas

---
+++ script.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+for i in {1,2,3,4,5,6,7,8,9,10}
+do
+
+#./nasteroid-par1 125 50 125 2000  >> par1-125-50-125-2000.txt
+#./nasteroid-par1 125 100 125 2000 >> par1-125-100-125-2000.txt
+#./nasteroid-par1 125 200 125 2000 >> par1-125-200-125-2000.txt
+#./nasteroid-par1 250 50 250 2000  >> par1-250-50-250-2000.txt
+#./nasteroid-par1 250 100 250 2000 >> par1-250-100-250-2000.txt
+#./nasteroid-par1 250 200 250 2000 >> par1-250-200-250-2000.txt
+#./nasteroid-par1 500 50 500 2000  >> par1-500-50-500-2000.txt
+#./nasteroid-par1 500 100 500 2000 >> par1-500-100-500-2000.txt
+#./nasteroid-par1 500 200 500 2000 >> par1-500-200-500-2000.txt
+
+#./nasteroid-par2 125 50 125 2000  >> par2-125-50-125-2000.txt
+#./nasteroid-par2 125 100 125 2000 >> par2-125-100-125-2000.txt
+#./nasteroid-par2 125 200 125 2000 >> par2-125-200-125-2000.txt
+#./nasteroid-par2 250 50 250 2000  >> par2-250-50-250-2000.txt
+#./nasteroid-par2 250 100 250 2000 >> par2-250-100-250-2000.txt
+#./nasteroid-par2 250 200 250 2000 >> par2-250-200-250-2000.txt
+#./nasteroid-par2 500 50 500 2000  >> par2-500-50-500-2000.txt
+#./nasteroid-par2 500 100 500 2000 >> par2-500-100-500-2000.txt
+#./nasteroid-par2 500 200 500 2000 >> par2-500-200-500-2000.txt
+
+#./nasteroid-par4 125 50 125 2000  >> par4-125-50-125-2000.txt
+#./nasteroid-par4 125 100 125 2000 >> par4-125-100-125-2000.txt
+#./nasteroid-par4 125 200 125 2000 >> par4-125-200-125-2000.txt
+#./nasteroid-par4 250 50 250 2000  >> par4-250-50-250-2000.txt
+#./nasteroid-par4 250 100 250 2000 >> par4-250-100-250-2000.txt
+#./nasteroid-par4 250 200 250 2000 >> par4-250-200-250-2000.txt
+#./nasteroid-par4 500 50 500 2000  >> par4-500-50-500-2000.txt
+#./nasteroid-par4 500 100 500 2000 >> par4-500-100-500-2000.txt
+#./nasteroid-par4 500 200 500 2000 >> par4-500-200-500-2000.txt
+
+#./nasteroid-par8 125 50 125 2000  >> par8-125-50-125-2000.txt
+#./nasteroid-par8 125 100 125 2000 >> par8-125-100-125-2000.txt
+#./nasteroid-par8 125 200 125 2000 >> par8-125-200-125-2000.txt
+#./nasteroid-par8 250 50 250 2000  >> par8-250-50-250-2000.txt
+#./nasteroid-par8 250 100 250 2000 >> par8-250-100-250-2000.txt
+#./nasteroid-par8 250 200 250 2000 >> par8-250-200-250-2000.txt
+#./nasteroid-par8 500 50 500 2000  >> par8-500-50-500-2000.txt
+#./nasteroid-par8 500 100 500 2000 >> par8-500-100-500-2000.txt
+#./nasteroid-par8 500 200 500 2000 >> par8-500-200-500-2000.txt
+
+./nasteroid-par16 125 50 125 2000  >> par16-125-50-125-2000.txt
+./nasteroid-par16 125 100 125 2000 >> par16-125-100-125-2000.txt
+./nasteroid-par16 125 200 125 2000 >> par16-125-200-125-2000.txt
+./nasteroid-par16 250 50 250 2000  >> par16-250-50-250-2000.txt
+./nasteroid-par16 250 100 250 2000 >> par16-250-100-250-2000.txt
+./nasteroid-par16 250 200 250 2000 >> par16-250-200-250-2000.txt
+./nasteroid-par16 500 50 500 2000  >> par16-500-50-500-2000.txt
+./nasteroid-par16 500 100 500 2000 >> par16-500-100-500-2000.txt
+./nasteroid-par16 500 200 500 2000 >> par16-500-200-500-2000.txt
+
+
+done
