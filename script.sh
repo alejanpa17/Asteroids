ff321dfc7bbe8962f1e95289f9e28d428310bfc3
alejanpa17 <alejandro170999@gmail.com>
2019-11-21 18:26:06 +0100

    Ultimas pruebas

--- script.sh
+++ script.sh
@@ -43,15 +43,74 @@ do
 #./nasteroid-par8 500 100 500 2000 >> par8-500-100-500-2000.txt
 #./nasteroid-par8 500 200 500 2000 >> par8-500-200-500-2000.txt
 
-./nasteroid-par16 125 50 125 2000  >> par16-125-50-125-2000.txt
-./nasteroid-par16 125 100 125 2000 >> par16-125-100-125-2000.txt
-./nasteroid-par16 125 200 125 2000 >> par16-125-200-125-2000.txt
-./nasteroid-par16 250 50 250 2000  >> par16-250-50-250-2000.txt
-./nasteroid-par16 250 100 250 2000 >> par16-250-100-250-2000.txt
-./nasteroid-par16 250 200 250 2000 >> par16-250-200-250-2000.txt
-./nasteroid-par16 500 50 500 2000  >> par16-500-50-500-2000.txt
-./nasteroid-par16 500 100 500 2000 >> par16-500-100-500-2000.txt
-./nasteroid-par16 500 200 500 2000 >> par16-500-200-500-2000.txt
+#./nasteroid-par16 125 50 125 2000  >> par16-125-50-125-2000.txt
+#./nasteroid-par16 125 100 125 2000 >> par16-125-100-125-2000.txt
+#./nasteroid-par16 125 200 125 2000 >> par16-125-200-125-2000.txt
+#./nasteroid-par16 250 50 250 2000  >> par16-250-50-250-2000.txt
+#./nasteroid-par16 250 100 250 2000 >> par16-250-100-250-2000.txt
+#./nasteroid-par16 250 200 250 2000 >> par16-250-200-250-2000.txt
+#./nasteroid-par16 500 50 500 2000  >> par16-500-50-500-2000.txt
+#./nasteroid-par16 500 100 500 2000 >> par16-500-100-500-2000.txt
+#./nasteroid-par16 500 200 500 2000 >> par16-500-200-500-2000.txt
 
+./nasteroid-par4dynamic 125 50 125 2000  >> par4dynamic-125-50-125-2000.txt
+./nasteroid-par4dynamic 125 100 125 2000 >> par4dynamic-125-100-125-2000.txt
+./nasteroid-par4dynamic 125 200 125 2000 >> par4dynamic-125-200-125-2000.txt
+./nasteroid-par4dynamic 250 50 250 2000  >> par4dynamic-250-50-250-2000.txt
+./nasteroid-par4dynamic 250 100 250 2000 >> par4dynamic-250-100-250-2000.txt
+./nasteroid-par4dynamic 250 200 250 2000 >> par4dynamic-250-200-250-2000.txt
+./nasteroid-par4dynamic 500 50 500 2000  >> par4dynamic-500-50-500-2000.txt
+./nasteroid-par4dynamic 500 100 500 2000 >> par4dynamic-500-100-500-2000.txt
+./nasteroid-par4dynamic 500 200 500 2000 >> par4dynamic-500-200-500-2000.txt
+
+./nasteroid-par8dynamic 125 50 125 2000  >> par8dynamic-125-50-125-2000.txt
+./nasteroid-par8dynamic 125 100 125 2000 >> par8dynamic-125-100-125-2000.txt
+./nasteroid-par8dynamic 125 200 125 2000 >> par8dynamic-125-200-125-2000.txt
+./nasteroid-par8dynamic 250 50 250 2000  >> par8dynamic-250-50-250-2000.txt
+./nasteroid-par8dynamic 250 100 250 2000 >> par8dynamic-250-100-250-2000.txt
+./nasteroid-par8dynamic 250 200 250 2000 >> par8dynamic-250-200-250-2000.txt
+./nasteroid-par8dynamic 500 50 500 2000  >> par8dynamic-500-50-500-2000.txt
+./nasteroid-par8dynamic 500 100 500 2000 >> par8dynamic-500-100-500-2000.txt
+./nasteroid-par8dynamic 500 200 500 2000 >> par8dynamic-500-200-500-2000.txt
+
+./nasteroid-par4guided 125 50 125 2000  >> par4guided-125-50-125-2000.txt
+./nasteroid-par4guided 125 100 125 2000 >> par4guided-125-100-125-2000.txt
+./nasteroid-par4guided 125 200 125 2000 >> par4guided-125-200-125-2000.txt
+./nasteroid-par4guided 250 50 250 2000  >> par4guided-250-50-250-2000.txt
+./nasteroid-par4guided 250 100 250 2000 >> par4guided-250-100-250-2000.txt
+./nasteroid-par4guided 250 200 250 2000 >> par4guided-250-200-250-2000.txt
+./nasteroid-par4guided 500 50 500 2000  >> par4guided-500-50-500-2000.txt
+./nasteroid-par4guided 500 100 500 2000 >> par4guided-500-100-500-2000.txt
+./nasteroid-par4guided 500 200 500 2000 >> par4guided-500-200-500-2000.txt
+
+./nasteroid-par8guided 125 50 125 2000  >> par8guided-125-50-125-2000.txt
+./nasteroid-par8guided 125 100 125 2000 >> par8guided-125-100-125-2000.txt
+./nasteroid-par8guided 125 200 125 2000 >> par8guided-125-200-125-2000.txt
+./nasteroid-par8guided 250 50 250 2000  >> par8guided-250-50-250-2000.txt
+./nasteroid-par8guided 250 100 250 2000 >> par8guided-250-100-250-2000.txt
+./nasteroid-par8guided 250 200 250 2000 >> par8guided-250-200-250-2000.txt
+./nasteroid-par8guided 500 50 500 2000  >> par8guided-500-50-500-2000.txt
+./nasteroid-par8guided 500 100 500 2000 >> par8guided-500-100-500-2000.txt
+./nasteroid-par8guided 500 200 500 2000 >> par8guided-500-200-500-2000.txt
+
+./nasteroid-par4static 125 50 125 2000  >> par4static-125-50-125-2000.txt
+./nasteroid-par4static 125 100 125 2000 >> par4static-125-100-125-2000.txt
+./nasteroid-par4static 125 200 125 2000 >> par4static-125-200-125-2000.txt
+./nasteroid-par4static 250 50 250 2000  >> par4static-250-50-250-2000.txt
+./nasteroid-par4static 250 100 250 2000 >> par4static-250-100-250-2000.txt
+./nasteroid-par4static 250 200 250 2000 >> par4static-250-200-250-2000.txt
+./nasteroid-par4static 500 50 500 2000  >> par4static-500-50-500-2000.txt
+./nasteroid-par4static 500 100 500 2000 >> par4static-500-100-500-2000.txt
+./nasteroid-par4static 500 200 500 2000 >> par4static-500-200-500-2000.txt
+
+./nasteroid-par8static 125 50 125 2000  >> par8static-125-50-125-2000.txt
+./nasteroid-par8static 125 100 125 2000 >> par8static-125-100-125-2000.txt
+./nasteroid-par8static 125 200 125 2000 >> par8static-125-200-125-2000.txt
+./nasteroid-par8static 250 50 250 2000  >> par8static-250-50-250-2000.txt
+./nasteroid-par8static 250 100 250 2000 >> par8static-250-100-250-2000.txt
+./nasteroid-par8static 250 200 250 2000 >> par8static-250-200-250-2000.txt
+./nasteroid-par8static 500 50 500 2000  >> par8static-500-50-500-2000.txt
+./nasteroid-par8static 500 100 500 2000 >> par8static-500-100-500-2000.txt
+./nasteroid-par8static 500 200 500 2000 >> par8static-500-200-500-2000.txt
 
 done
